49d1c85504c9999cb6721f03672384ba5df2bf0b
Wei Dai <cs15lsp22akb@ieng6-201.ucsd.edu>
2022-05-01 15:44:24 -0700

    Debug

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public int[] sum(int[] nums,int k){
         int[] result = new int[2];
         for(int i=0;i<nums.length;i++){
-            for(int j=nums.length;i<j;j--){
+            for(int j=nums.length-1;i<j;j--){
                 if(k==nums[i]+nums[j]){
                     result[0]=nums[i];
                     result[1]=nums[j];
